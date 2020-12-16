a6c6d19061ac1d8545fdc576c197013ca015013f
lghiggino <lghiggino@gmail.com>
2020-12-16 11:25:29 -0300

    Holy molly, its almost done

---
+++ assets/js/multijob.js
@@ -0,0 +1,6 @@
+let jobs = [ "Front-end developer", "Full-Stack Software Engineer", "Growth hacker", "Video editor enthusiast"]
+
+const multijob = document.querySelector("#multijob");
+
+
+multijob.innerText = jobs[0]
