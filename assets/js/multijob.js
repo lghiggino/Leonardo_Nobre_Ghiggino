a5282561716de00cf4898abd1a128995e4983b53
lghiggino <lghiggino@gmail.com>
2020-12-16 16:29:56 -0300

    cleaning it up

--- assets/js/multijob.js
+++ assets/js/multijob.js
@@ -1,6 +1,21 @@
-let jobs = [ "Front-end developer", "Full-Stack Software Engineer", "Growth hacker", "Video editor enthusiast"]
+let jobs = [ "Front-end developer", "Full-Stack Software Engineer", "Growth hacker", "Video editing enthusiast"]
 
 const multijob = document.querySelector("#multijob");
 
-
 multijob.innerText = jobs[0]
+
+// let i = -1
+// function arraySelector(jobs){
+//     if(i > jobs.length){
+//         i = -1
+//         i++
+//         return multijob.innerText = jobs[i]
+//     } else {
+//         i++
+//         multijob.innerText = ""
+//         return multijob.innerText = jobs[i]
+//     }
+// }
+
+//setInterval(arraySelector(jobs), 5000)
+  
